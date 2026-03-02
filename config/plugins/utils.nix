dd4e9de97ed8c29029eafda74bc59fa5640f8c53
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-03 02:29:01 +0900

    wip

--- config/plugins/utils.nix
+++ config/plugins/utils.nix
@@ -3,15 +3,4 @@
   plugins.todo-comments = {
     enable = true;
   };
-
-  # discord rich presence
-  plugins.neocord = {
-    enable = true;
-    lazyLoad.settings.event = ["VeryLazy"];
-    settings = {
-      logo = "auto";
-      main_image = "logo";
-      show_time = true;
-    };
-  };
 }
