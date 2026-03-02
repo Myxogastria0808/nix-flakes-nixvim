32cec0e5bd6c305856d264e26092976437ceddcc
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-03 02:14:49 +0900

    chore: add discord rich presence

--- config/plugins/utils.nix
+++ config/plugins/utils.nix
@@ -2,4 +2,16 @@
   plugins.todo-comments = {
     enable = true;
   };
+
+  # Discord Rich Presence
+  plugins.neocord = {
+    enable = true;
+    settings = {
+      logo = "auto";
+      main_image = "logo";
+      show_time = true;
+      enable_line_number = true;
+      global_timer = true;
+    };
+  };
 }
