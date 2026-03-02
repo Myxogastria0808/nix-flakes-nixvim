5a69c46d51a3737e2b6a3427e41c7b11a42864f3
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-02 21:05:13 +0900

    wip

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -15,7 +15,18 @@
     # lean
     lean = {
       enable = true;
-      settings.infoview.width = 0.2;
+      settings = {
+        # infoview (proof state window) settings
+        infoview.width = 0.2;
+        # enable recommended keymaps in Lean buffers
+        # <leader>i : toggle infoview open/close
+        # <leader>p : pause/unpause infoview updates
+        # <leader>x : add pin at current cursor position
+        # <leader>c : clear all pins
+        # <leader>d : set diff pin (compare two proof states)
+        # <leader>t : replace "sorry" with suggestion (Try this)
+        mappings = true;
+      };
     };
   };
 }
