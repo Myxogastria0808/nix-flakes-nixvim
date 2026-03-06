262ae8f69bac7fb1e7cff73beee3dc8789bbc219
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-06 23:00:25 +0900

    wip

--- config/libs/ui/dashboard.nix
+++ config/libs/ui/dashboard.nix
@@ -2,13 +2,8 @@
   # alpha-nvim
   # reference: https://github.com/goolord/alpha-nvim
   #
-  # Displays a rich start screen (dashboard) when Neovim is opened without
-  # file arguments. Provides quick-access buttons and a Neovim logo header.
-  #
-  # Button shortcuts (on the dashboard):
-  # n : new empty buffer
-  # e : file explorer (Neo-tree)
-  # q : quit Neovim
+  # Displays a start screen with the Neovim logo and version when opened
+  # without file arguments.
   plugins.alpha = {
     enable = true;
     settings.layout = [
@@ -37,58 +32,6 @@
         val = 2;
       }
       {
-        type = "group";
-        val = [
-          {
-            type = "button";
-            val = "  New File";
-            on_press.__raw = "function() vim.cmd('enew') end";
-            opts = {
-              shortcut = "n";
-              position = "center";
-              width = 44;
-              hl = "AlphaButton";
-              hl_shortcut = "AlphaButtonShortcut";
-              cursor = 3;
-              align_shortcut = "right";
-            };
-          }
-          {
-            type = "button";
-            val = "  File Explorer";
-            on_press.__raw = "function() vim.cmd('Neotree toggle') end";
-            opts = {
-              shortcut = "e";
-              position = "center";
-              width = 44;
-              hl = "AlphaButton";
-              hl_shortcut = "AlphaButtonShortcut";
-              cursor = 3;
-              align_shortcut = "right";
-            };
-          }
-          {
-            type = "button";
-            val = "  Quit";
-            on_press.__raw = "function() vim.cmd('qa') end";
-            opts = {
-              shortcut = "q";
-              position = "center";
-              width = 44;
-              hl = "AlphaButton";
-              hl_shortcut = "AlphaButtonShortcut";
-              cursor = 3;
-              align_shortcut = "right";
-            };
-          }
-        ];
-        opts.spacing = 1;
-      }
-      {
-        type = "padding";
-        val = 2;
-      }
-      {
         # Footer: Neovim version, dynamically rendered via raw Lua
         type = "text";
         val.__raw = ''
@@ -111,16 +54,6 @@
     AlphaNvimHeader = {
       fg = "#7aa2f7";
       bold = true;
-    };
-    # Button text: soft foreground
-    AlphaButton = {
-      fg = "#c0caf5";
-    };
-    # Button shortcut key: orange accent
-    AlphaButtonShortcut = {
-      fg = "#ff9e64";
-      bold = true;
-      italic = true;
     };
     # Footer: muted comment color
     AlphaFooter = {
