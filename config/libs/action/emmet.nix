8156ff512679977d973a0ad66ab0fae6e7fee2bf
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-07 16:23:43 +0900

    wip

--- config/libs/action/emmet.nix
+++ config/libs/action/emmet.nix
@@ -8,19 +8,13 @@
   #
   # Keymaps (All modes):
   # <C-y>, : expand abbreviation
-  plugins.emmet = {
-    enable = true;
-    mode = "a";
-    leader_key = "<C-y>";
-  };
+  plugins.emmet.enable = true;
 
-  extraConfigLua = ''
-    vim.g.user_emmet_settings = {
-      html = {
-        snippets = {
-          ["!"] = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Document</title>\n</head>\n<body>\n    \n</body>\n</html>"
-        }
-      }
-    }
-  '';
+  globals = {
+    user_emmet_mode = "a";
+    user_emmet_leader_key = "<C-y>";
+    user_emmet_settings = {
+      html.snippets."!" = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Document</title>\n</head>\n<body>\n    \n</body>\n</html>";
+    };
+  };
 }
