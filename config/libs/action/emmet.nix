e653de7f1b0b64822c2b4dc2c7e5f39e3a3f15cb
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-08 17:31:24 +0900

    wip

--- config/libs/action/emmet.nix
+++ config/libs/action/emmet.nix
@@ -11,11 +11,13 @@
   plugins.emmet = {
     enable = true;
     settings = {
-      user_emmet_leader_key = "<C-y>";
-      user_emmet_mode = "a";
-      html = {
-        snippets = {
-          "html:5" = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"UTF-8\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>Document</title>\n</head>\n<body>\n\t|\n</body>\n</html>";
+      leader_key = "<C-y>";
+      mode = "a";
+      settings = {
+        html = {
+          snippets = {
+            "html:5" = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"UTF-8\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>Document</title>\n</head>\n<body>\n\t|\n</body>\n</html>";
+          };
         };
       };
     };
